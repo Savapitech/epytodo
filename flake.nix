238adeb81da74e12dd1c68c7190f9e47aba8db6b
savalet <savinien.petitjean@gmail.com>
2025-05-11 18:18:35 +0200

    Init

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    forAllSystems = function:
+      nixpkgs.lib.genAttrs [
+        "x86_64-linux"
+        "aarch64-linux"
+      ] (system: function nixpkgs.legacyPackages.${system});
+  in {
+    devShells = forAllSystems (pkgs: {
+      default = pkgs.mkShell {
+        hardeningDisable = ["fortify"];
+        packages = with pkgs; [
+          nodejs_24
+        ];
+      };
+    });
+  };
+}
