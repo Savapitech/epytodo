7521c45713573de3a4255bc235782f7683f9ec9d
savalet <savinien.petitjean@gmail.com>
2025-05-11 19:43:53 +0200

    Add basic index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("Hello World !");
+const express = require('express')
+require('dotenv').config()
+
+const app = express()
+const version = require('../package.json').version
+
+console.log(`\x1b[0;36m EpyTodo API | v${version}\x1b[0;37m`)
+
+app.listen(process.env.PORT)
+console.log(`The API is listening on port ${process.env.PORT}`)
