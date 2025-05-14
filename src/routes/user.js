9d31ca39fd9af0b46f4ca33eb99703b7228b21f8
eiffis <enzo.iffis@epitech.eu>
2025-05-14 15:25:37 +0200

    Add GET /user/todos

--- src/routes/user.js
+++ src/routes/user.js
@@ -20,4 +20,21 @@ router.get('/', auth, async (req, res) => {
     }
 });
 
+
+router.get('/todos', auth, async (req, res) => {
+    try {
+        const [rows] = await pool.query("SELECT *, DATE_FORMAT(CONVERT_TZ(due_time, '+00:00', '+00:00'), '%Y-%m-%d %H:%i:%s') AS due_time, DATE_FORMAT(CONVERT_TZ(created_at, '+00:00', '+00:00'), '%Y-%m-%d %H:%i:%s') AS created_at FROM todo WHERE user_id = ?", [req.user.id]);
+
+        if (rows.length < 1)
+            return res.status(404).json({ msg: 'Not found' });
+
+        res.json(rows[0]);
+    } catch (err) {
+        console.error('Database error:', err);
+        res.status(500).json({ msg: 'Internal server error' });
+    }
+});
+
+
+
 module.exports = router;
